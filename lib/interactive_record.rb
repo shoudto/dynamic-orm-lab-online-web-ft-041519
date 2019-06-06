308384a375774ba2637eabf6b82c5e92b66c3b58
Oscar Shoudt <shoudto@gmail.com>
2019-06-06 20:23:49 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -26,10 +26,12 @@ class InteractiveRecord
       self.send("#{project}=", value)
     end 
   end 
-
   
-  # def table_name_for_insert
-  #   self.class.table_name
-  # end 
+  def table_name_for_insert
+    self.class.table_name
+  end 
   
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end 
 end
